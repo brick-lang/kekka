cf185128ff1ef47293413ac90a485cefcde1baa7
Katherine Whitlock <toroidalcode@gmail.com>
2018-01-19 17:15:15 -0500

    Move expr.ml(i) to misc

--- kindEngine/synonym.ml
+++ kindEngine/synonym.ml
@@ -2,4 +2,4 @@
 
 (* Synonyms: a map from type synonym names to a tuple of a synonym
  * type scheme ('SynInfo'), an integer that gives the relative _rank_ of the type synonym *)
-(* type synonyms = syn_info Common.Name.Map.t *)
+type synonyms = Heart.Type.syn_info Common.Name.Map.t
